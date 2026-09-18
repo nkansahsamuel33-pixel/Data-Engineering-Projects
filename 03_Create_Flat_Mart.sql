9b2c618d78cf899cfb853c1620dbff22958bff9b
nkansahsamuel33-pixel <nkansahsamuel33@gmail.com>
2026-09-18 03:41:36 +0000

    flat mart created

---
+++ 03_Create_Flat_Mart.sql
@@ -0,0 +1,49 @@
+DROP SCHEMA IF EXISTS flat_mart CASCADE;
+CREATE SCHEMA flat_mart;
+
+SELECT '== Loading flat mart job postings ==' AS info;
+
+CREATE OR REPLACE flat_mart.job_postings AS
+SELECT
+    jpf.job_id,
+    jpf.company_id,
+    jpf.job_title_short,
+    jpf.job_location,
+    jpf.job_via,
+    jpf.job_schedule_type,
+    jpf.job_work_from_home,
+    jpf.search_location,
+    jpf.job_posted_date,
+    jpf.job_no_degree_mention,
+    jpf.job_health_insurance,
+    jpf.job_country,
+    jpf.salary_rate,
+    jpf.salary_year_avg,
+    jpf.salary_hour_avg,
+    cd.company_id,
+    cd.name AS company_name,
+    ARRAY_AGG(
+        STRUCT_PACK (
+            type := sd.type,
+            name := sd.skills
+        )
+    ) AS skills_and_types
+
+FROM job_postings_fact AS jpf 
+LEFT JOIN  company_dim AS cd
+ON jpf.company_id = cd.company_id
+
+LEFT JOIN  skills_job_dim_dim AS skd
+ON jpf.job_id = sjd.job_id
+
+LEFT JOIN skills_dim AS sd 
+ON sjd.skill_id = sd.skill_id
+
+GROUP BY ALL;
+
+-- Data Validation Check
+
+SELECT 'flat mart job postings' AS table_name COUNT(*) AS record_count FROM flat_mart.job_postings
+
+SELECT '== flat mart job postings sample ==' AS info;
+SELECT * FROM flat_mart.job_postings LIMIT 10;
--- build_marts.sql
+++ build_marts.sql
@@ -4,8 +4,8 @@
 .read Create_Tables.sql
 
 -- STEP 2 Load data from CSV files into tables
-
 .read Load_schema.sql 
 
-
+-- STEP 3 Create flat_mart
+.read 03_Create_Flat_Mart.sql
 
